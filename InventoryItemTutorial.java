ccf92d0d5f86355b7897fb673cdc1b4d36dba882
coolAlias <BASbliznet@gmail.com>
2013-08-25 13:30:11 +0900

    Rename InventoryItemTutorial.txt to InventoryItemTutorial.java

--- InventoryItemTutorial.java
+++ InventoryItemTutorial.java
@@ -1,6 +1,6 @@
 /**
  * Creating an Item that stores an Inventory (such as a Backpack)
- * (and also “How to Properly Override Shift-Clicking” - see Step 3)
+ * (and also ï¿½How to Properly Override Shift-Clickingï¿½ - see Step 3)
  */
 /*
 I'm back with a new tutorial on how to create an Item that can store an Inventory,
@@ -350,7 +350,7 @@ There's a LOT of code in this one, but read through all of the comments carefull
 and it should become clear what everything does.
 
 As a bonus, one of my previous tutorials is included within!
-“How to Properly Override Shift-Clicking” is all here and better than ever!
+ï¿½How to Properly Override Shift-Clickingï¿½ is all here and better than ever!
 At least in my opinion.
 
 If you're like me, and you find no end of frustration trying to figure out which
@@ -716,8 +716,8 @@ public class GuiItemInventory extends GuiContainer
 
 	public GuiItemInventory(ContainerItem containerItem)
 	{
-		     super(containerItem);
-		     this.inventory = containerItem.inventory;
+		ï¿½ï¿½ï¿½ï¿½ super(containerItem);
+		ï¿½ï¿½ï¿½ï¿½ this.inventory = containerItem.inventory;
 	}
 
 	/**
@@ -725,9 +725,9 @@ public class GuiItemInventory extends GuiContainer
 	 */
 	public void drawScreen(int par1, int par2, float par3)
 	{
-		     super.drawScreen(par1, par2, par3);
-		     this.xSize_lo = (float)par1;
-		     this.ySize_lo = (float)par2;
+		ï¿½ï¿½ï¿½ï¿½ super.drawScreen(par1, par2, par3);
+		ï¿½ï¿½ï¿½ï¿½ this.xSize_lo = (float)par1;
+		ï¿½ï¿½ï¿½ï¿½ this.ySize_lo = (float)par2;
 	}
 
 	/**
@@ -735,9 +735,9 @@ public class GuiItemInventory extends GuiContainer
 	 */
 	protected void drawGuiContainerForegroundLayer(int par1, int par2)
 	{
-		     String s = this.inventory.isInvNameLocalized() ? this.inventory.getInvName() : I18n.func_135053_a(this.inventory.getInvName());
-		     this.fontRenderer.drawString(s, this.xSize / 2 - this.fontRenderer.getStringWidth(s) / 2, 0, 4210752);
-		     this.fontRenderer.drawString(I18n.func_135053_a("container.inventory"), 26, this.ySize - 96 + 4, 4210752);
+		ï¿½ï¿½ï¿½ï¿½ String s = this.inventory.isInvNameLocalized() ? this.inventory.getInvName() : I18n.func_135053_a(this.inventory.getInvName());
+		ï¿½ï¿½ï¿½ï¿½ this.fontRenderer.drawString(s, this.xSize / 2 - this.fontRenderer.getStringWidth(s) / 2, 0, 4210752);
+		ï¿½ï¿½ï¿½ï¿½ this.fontRenderer.drawString(I18n.func_135053_a("container.inventory"), 26, this.ySize - 96 + 4, 4210752);
 	}
 
 	/**
@@ -745,13 +745,13 @@ public class GuiItemInventory extends GuiContainer
 	 */
 	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
 	{
-		     GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		     this.mc.func_110434_K().func_110577_a(iconLocation);
-		     int k = (this.width - this.xSize) / 2;
-		     int l = (this.height - this.ySize) / 2;
-		     this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
-		     int i1;
-		     drawPlayerModel(k + 51, l + 75, 30, (float)(k + 51) - this.xSize_lo, (float)(l + 75 - 50) - this.ySize_lo, this.mc.thePlayer);
+		ï¿½ï¿½ï¿½ï¿½ GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		ï¿½ï¿½ï¿½ï¿½ this.mc.func_110434_K().func_110577_a(iconLocation);
+		ï¿½ï¿½ï¿½ï¿½ int k = (this.width - this.xSize) / 2;
+		ï¿½ï¿½ï¿½ï¿½ int l = (this.height - this.ySize) / 2;
+		ï¿½ï¿½ï¿½ï¿½ this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
+		ï¿½ï¿½ï¿½ï¿½ int i1;
+		ï¿½ï¿½ï¿½ï¿½ drawPlayerModel(k + 51, l + 75, 30, (float)(k + 51) - this.xSize_lo, (float)(l + 75 - 50) - this.ySize_lo, this.mc.thePlayer);
 	}
 
 	/**
@@ -759,39 +759,39 @@ public class GuiItemInventory extends GuiContainer
 	 */
 	public static void drawPlayerModel(int par0, int par1, int par2, float par3, float par4, EntityLivingBase par5EntityLivingBase)
 	{
-		     GL11.glEnable(GL11.GL_COLOR_MATERIAL);
-		     GL11.glPushMatrix();
-		     GL11.glTranslatef((float)par0, (float)par1, 50.0F);
-		     GL11.glScalef((float)(-par2), (float)par2, (float)par2);
-		     GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-		     float f2 = par5EntityLivingBase.renderYawOffset;
-		     float f3 = par5EntityLivingBase.rotationYaw;
-		     float f4 = par5EntityLivingBase.rotationPitch;
-		     float f5 = par5EntityLivingBase.prevRotationYawHead;
-		     float f6 = par5EntityLivingBase.rotationYawHead;
-		     GL11.glRotatef(135.0F, 0.0F, 1.0F, 0.0F);
-		     RenderHelper.enableStandardItemLighting();
-		     GL11.glRotatef(-135.0F, 0.0F, 1.0F, 0.0F);
-		     GL11.glRotatef(-((float)Math.atan((double)(par4 / 40.0F))) * 20.0F, 1.0F, 0.0F, 0.0F);
-		     par5EntityLivingBase.renderYawOffset = (float)Math.atan((double)(par3 / 40.0F)) * 20.0F;
-		     par5EntityLivingBase.rotationYaw = (float)Math.atan((double)(par3 / 40.0F)) * 40.0F;
-		     par5EntityLivingBase.rotationPitch = -((float)Math.atan((double)(par4 / 40.0F))) * 20.0F;
-		     par5EntityLivingBase.rotationYawHead = par5EntityLivingBase.rotationYaw;
-		     par5EntityLivingBase.prevRotationYawHead = par5EntityLivingBase.rotationYaw;
-		     GL11.glTranslatef(0.0F, par5EntityLivingBase.yOffset, 0.0F);
-		     RenderManager.instance.playerViewY = 180.0F;
-		     RenderManager.instance.renderEntityWithPosYaw(par5EntityLivingBase, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F);
-		     par5EntityLivingBase.renderYawOffset = f2;
-		     par5EntityLivingBase.rotationYaw = f3;
-		     par5EntityLivingBase.rotationPitch = f4;
-		     par5EntityLivingBase.prevRotationYawHead = f5;
-		     par5EntityLivingBase.rotationYawHead = f6;
-		     GL11.glPopMatrix();
-		     RenderHelper.disableStandardItemLighting();
-		     GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-		     OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit);
-		     GL11.glDisable(GL11.GL_TEXTURE_2D);
-		     OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
+		ï¿½ï¿½ï¿½ï¿½ GL11.glEnable(GL11.GL_COLOR_MATERIAL);
+		ï¿½ï¿½ï¿½ï¿½ GL11.glPushMatrix();
+		ï¿½ï¿½ï¿½ï¿½ GL11.glTranslatef((float)par0, (float)par1, 50.0F);
+		ï¿½ï¿½ï¿½ï¿½ GL11.glScalef((float)(-par2), (float)par2, (float)par2);
+		ï¿½ï¿½ï¿½ï¿½ GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+		ï¿½ï¿½ï¿½ï¿½ float f2 = par5EntityLivingBase.renderYawOffset;
+		ï¿½ï¿½ï¿½ï¿½ float f3 = par5EntityLivingBase.rotationYaw;
+		ï¿½ï¿½ï¿½ï¿½ float f4 = par5EntityLivingBase.rotationPitch;
+		ï¿½ï¿½ï¿½ï¿½ float f5 = par5EntityLivingBase.prevRotationYawHead;
+		ï¿½ï¿½ï¿½ï¿½ float f6 = par5EntityLivingBase.rotationYawHead;
+		ï¿½ï¿½ï¿½ï¿½ GL11.glRotatef(135.0F, 0.0F, 1.0F, 0.0F);
+		ï¿½ï¿½ï¿½ï¿½ RenderHelper.enableStandardItemLighting();
+		ï¿½ï¿½ï¿½ï¿½ GL11.glRotatef(-135.0F, 0.0F, 1.0F, 0.0F);
+		ï¿½ï¿½ï¿½ï¿½ GL11.glRotatef(-((float)Math.atan((double)(par4 / 40.0F))) * 20.0F, 1.0F, 0.0F, 0.0F);
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.renderYawOffset = (float)Math.atan((double)(par3 / 40.0F)) * 20.0F;
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.rotationYaw = (float)Math.atan((double)(par3 / 40.0F)) * 40.0F;
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.rotationPitch = -((float)Math.atan((double)(par4 / 40.0F))) * 20.0F;
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.rotationYawHead = par5EntityLivingBase.rotationYaw;
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.prevRotationYawHead = par5EntityLivingBase.rotationYaw;
+		ï¿½ï¿½ï¿½ï¿½ GL11.glTranslatef(0.0F, par5EntityLivingBase.yOffset, 0.0F);
+		ï¿½ï¿½ï¿½ï¿½ RenderManager.instance.playerViewY = 180.0F;
+		ï¿½ï¿½ï¿½ï¿½ RenderManager.instance.renderEntityWithPosYaw(par5EntityLivingBase, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F);
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.renderYawOffset = f2;
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.rotationYaw = f3;
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.rotationPitch = f4;
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.prevRotationYawHead = f5;
+		ï¿½ï¿½ï¿½ï¿½ par5EntityLivingBase.rotationYawHead = f6;
+		ï¿½ï¿½ï¿½ï¿½ GL11.glPopMatrix();
+		ï¿½ï¿½ï¿½ï¿½ RenderHelper.disableStandardItemLighting();
+		ï¿½ï¿½ï¿½ï¿½ GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+		ï¿½ï¿½ï¿½ï¿½ OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+		ï¿½ï¿½ï¿½ï¿½ GL11.glDisable(GL11.GL_TEXTURE_2D);
+		ï¿½ï¿½ï¿½ï¿½ OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
 	}
 }
 
