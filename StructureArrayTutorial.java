d02c7124c71a2303efdef8e5a42e7acdbc2ab9f5
coolAlias <coolAlias@users.noreply.github.com>
2013-09-29 17:21:04 +0900

    Added StructureArrayTutorial
    For the assistance of people using my StructureGenTool Mod :D

--- StructureArrayTutorial.java
+++ StructureArrayTutorial.java
@@ -1,22 +1,3 @@
-/**
-    Copyright (C) <2013> <coolAlias>
-
-    This file is part of coolAlias' Structure Generation Tool; as such,
-    you can redistribute it and/or modify it under the terms of the GNU
-    General Public License as published by the Free Software Foundation,
-    either version 3 of the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-package coolalias.structuregen;
-
 /*
 In this tutorial I will cover how to build your structure using an array from scratch.
 Please also refer to the instructions in 'StructureArrays.java' for more information.
